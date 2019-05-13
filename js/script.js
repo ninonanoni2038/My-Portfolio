8c1b2641db67bf2882c36a263cfbe4e440b8cbe4
Ninom <nionanoni2038@gmail.com>
2019-05-13 23:17:35 +0900

    svg animation

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*global $*/
+
+
+$(function () {
+    "use strict";
+    setTimeout(function () {
+        $('.message').fadeOut(1000);
+        $('.message-background').animate({'top':'-800px'},1000);
+    }, 3000);
+    setTimeout(function () {
+     $('.menu-bar').fadeIn(1000);
+    }, 3500);
+    
+});
