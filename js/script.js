0589681ffc247222405d1d53570d56336fbc4644
Ninom <nionanoni2038@gmail.com>
2019-05-14 21:54:26 +0900

    add about and skill

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ $(function () {
     "use strict";
     setTimeout(function () {
         $('.message').fadeOut(1000);
-        $('.message-background').animate({'top':'-800px'},1000);
-    }, 3000);
-    setTimeout(function () {
-     $('.menu-bar').fadeIn(1000);
+        $('.message-background').animate({
+            'top': '-800px'
+        }, 1000);
     }, 3500);
-    
+    setTimeout(function () {
+        $('.menu-bar').fadeIn(1000);
+    }, 4000);
 });
