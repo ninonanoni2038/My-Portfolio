0a4387dee2f8ecaf042d8ad09bd692c634150c1c
Ninom <ninonanoni2038@gmail.com>
2019-05-16 00:35:35 +0900

    add about and vision

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ $(function () {
         $('.menu-bar').fadeIn(1000);
     }, 4000);
 });
+
+$(function(){
+    $('a[href^=#]').click(function(){
+        
+        var speed = 500,
+
+            href= $(this).attr("href"),
+
+            target = $(href == "#" || href == "" ? 'html' : href),
+
+            position = target.offset().top,
+
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+
+        return false;
+
+    });
+
+});
